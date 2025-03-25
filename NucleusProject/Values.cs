d795bad190a35c24605ea73801b4b7213cbabd49
BoseV <46490582+BoseVJeff@users.noreply.github.com>
2025-03-25 13:24:15 +0530

    Add project files.

---
+++ NucleusProject/Values.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace NucleusProject
+{
+    public class Values
+    {
+        static readonly string connString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\jeffb\Desktop\dev\csharp\NucleusProject\NucleusProject\App_Data\NucleusDatabase.mdf;Integrated Security=True";
+
+        private static readonly string baseDir = @"C:\Users\jeffb\Desktop\dev\csharp\NucleusProject\NucleusProject\";
+
+        public static string ConnectionString { get {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = @"(LocalDB)\MSSQLLocalDB";
+                builder.AttachDBFilename = baseDir+ "App_Data\\NucleusDatabase.mdf";
+                builder.IntegratedSecurity= true;
+
+                return builder.ToString();
+            } }
+
+    }
+}
