c9b65e337c043636104cf4f0381fa2ca7d4fe666
BoseV <46490582+BoseVJeff@users.noreply.github.com>
2025-03-29 21:07:53 +0530

    Fixed download functionality

--- NucleusProject/Report.aspx
+++ NucleusProject/Report.aspx
@@ -1,4 +1,4 @@
 ﻿<%@ Page Title="Report" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Report.aspx.cs" Inherits="NucleusProject.Report" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
-    <asp:Button runat="server" Text="Download Report" CssClass="btn btn-primary" />
+    <asp:Button runat="server" Text="Download Report" CssClass="btn btn-primary" OnClick="Unnamed_Click" />
 </asp:Content>
--- NucleusProject/Report.aspx.cs
+++ NucleusProject/Report.aspx.cs
@@ -12,19 +12,43 @@ namespace NucleusProject
         string path = @"~/favicon.ico";
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (Session["id"] == null)
+            // TODO: Enable this once actual report generation and download is figured out
+            //if (Session["id"] == null)
+            //{
+                //Response.Redirect("~/");
+            //}
+        }
+
+        protected string ResolvePath(string path)
+        {
+            try
             {
-                Response.Redirect("~/");
+                return Server.MapPath(path);
+            } catch (HttpException)
+            {
+                // Happens with concrete paths
+                return path;
             }
         }
 
-        protected void downloadFile()
+        protected void DownloadFile()
         {
-            // Taken from https://stackoverflow.com/a/21417259
+
+            // Adapted from https://stackoverflow.com/a/21417259
+            string file = ResolvePath(this.path);
+            System.IO.FileInfo fileInfo = new System.IO.FileInfo(file);
             Response.ContentType = "application/pdf";
             Response.AppendHeader("Content-Disposition", "attatchment;filename=report.pdf");
-            Response.TransmitFile(this.path);
+            Response.AppendHeader("Content-Length",fileInfo.Length.ToString());
+            Response.TransmitFile(file);
+            // Probably unecessary, keeping it anyway
+            Response.Flush();
             Response.End();
+        }
+
+        protected void Unnamed_Click(object sender, EventArgs e)
+        {
+            DownloadFile();
         }
     }
 }
