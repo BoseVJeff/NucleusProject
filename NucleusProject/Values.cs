3988688b7ac0654ce079f2e658f5e95b45e95d59
BoseV <46490582+BoseVJeff@users.noreply.github.com>
2025-04-11 01:02:00 +0530

    Added `TimeDuration` and `StudentId()`

--- NucleusProject/Values.cs
+++ NucleusProject/Values.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Hosting;
+using System.Web.SessionState;
 
 namespace NucleusProject
 {
@@ -14,6 +15,64 @@ namespace NucleusProject
         Week,
         Month,
         Custom
+    }
+    public class TimeDuration
+    {
+        public DateTimeOffset start;
+        public DateTimeOffset end;
+
+        public TimeDuration(DateTimeOffset startDateTimeOffset, DateTimeOffset endDateTimeOffset)
+        {
+            this.start = startDateTimeOffset;
+            this.end = endDateTimeOffset;
+        }
+
+        public static TimeDuration AroundDateTime(DateTimeOffset? dateTimeOffset,ViewSpan span) {
+            // Setting a sensible default
+            DateTimeOffset date = dateTimeOffset ?? DateTimeOffset.Now;
+
+            // Holders for eventual values
+            DateTimeOffset startDate;
+            DateTimeOffset endDate;
+
+            // Set start and end dates based on the span
+            switch (span)
+            {
+                case ViewSpan.Day:
+                    // Start at first minute of the current day
+                    startDate= new DateTimeOffset(year: date.Year, month: date.Month, day: date.Day, hour: 0, minute: 0,second: 0, offset: date.Offset);
+                    // End at the last minute of the current day
+                    endDate= new DateTimeOffset(year: date.Year, month: date.Month, day: date.Day, hour: 23, minute: 59, second: 59, offset: date.Offset);
+                    break;
+                case ViewSpan.Week:
+                    // Week starts on Sunday
+                    // TODO: Customisable start of week? Will require additional tests to verify functionality
+
+                    // Start at first minute of the first day of the current week
+                    startDate = new DateTimeOffset(year: date.Year, month: date.Month, day: date.Day-((int)date.DayOfWeek)+((int)DayOfWeek.Sunday), hour: 0, minute: 0, second: 0, offset: date.Offset);
+                    // End at the last minute of the last day of the current week
+                    endDate = new DateTimeOffset(year: date.Year, month: date.Month, day: date.Day - (int)date.DayOfWeek + (int)DayOfWeek.Saturday, hour: 23, minute: 59, second: 59, offset: date.Offset);
+                    break;
+                case ViewSpan.Month:
+                    // Start at first day of the current month
+                    startDate = new DateTimeOffset(year: date.Year, month: date.Month, day: 1, hour: 0, minute: 0, second: 0, offset: date.Offset);
+                    // End at the last day of the current month
+                    endDate = new DateTimeOffset(year: date.Year, month: date.Month, day: DateTime.DaysInMonth(date.Year,date.Month), hour: 23, minute: 59, second: 59, offset: date.Offset);
+                    break;
+                default:
+                    // For unknown spans
+                    startDate = DateTime.Now;
+                    endDate = DateTime.Now;
+                    break;
+            }
+
+            return new TimeDuration(startDate, endDate);
+        }
+
+        public override string ToString()
+        {
+            return start.ToString() + " - "+ end.ToString();
+        }
     }
     public class Values
     {
@@ -27,6 +86,48 @@ namespace NucleusProject
 
                 return builder.ToString();
             } }
+
+        public static int? StudentId(HttpSessionState session, HttpCookieCollection cookies)
+        {
+            // Check session for value first, then cookies, else return null
+            // Seaching for key `id`
+
+            if (session["id"] != null)
+            {
+                return (int)session["id"];
+            } else
+            {
+                // No current session, maybe because the user has not logged in
+                // Fallthrough to cookie id
+            }
+
+            if (cookies != null)
+            {
+                if (cookies["id"] != null)
+                {
+                    int outId;
+                    if (Int32.TryParse(cookies["id"].Value, out outId))
+                    {
+                        return outId;
+                    }
+                    else
+                    {
+                        // Failed to parse cookie
+                        // TODO: Maybe log here?
+                        return null;
+                    }
+                }
+                else
+                {
+                    // No `id` cookie found
+                    return null;
+                }
+            } else
+            {
+                // No cookies
+                return null;
+            }
+        }
 
     }
 }
