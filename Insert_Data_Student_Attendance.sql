9ee6dac277b164e913d351c5b232b58c96ae5a36
BoseV <46490582+BoseVJeff@users.noreply.github.com>
2025-04-13 22:06:14 +0530

    Added script to add attendance data

---
+++ Insert_Data_Student_Attendance.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO Map_Trn_Schedule_Student_Attendance (Student,Schedule,Attendance)
+SELECT 3, F16, F17
+FROM OPENROWSET(
+    'Microsoft.ACE.OLEDB.12.0',
+    'Excel 12.0;HDR=NO;Database=C:\Users\jeffb\Desktop\Nucleus_Schedule_Data.xlsx',
+    'select F16, F17 from [Sheet3$] WHERE NOT ISNULL(F17)' ) ORDER BY (SELECT 0) OFFSET 0 ROWS FETCH NEXT 482 ROWS ONLY
