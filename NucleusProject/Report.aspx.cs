3ba1287e052d9d5a6b57260ce3ca3611c8ac54a5
BoseV <46490582+BoseVJeff@users.noreply.github.com>
2025-04-16 17:53:50 +0530

    Fixed broken merge

--- NucleusProject/Report.aspx.cs
+++ NucleusProject/Report.aspx.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 
 namespace NucleusProject
@@ -12,36 +14,89 @@ namespace NucleusProject
         string path = @"~/favicon.ico";
         protected void Page_Load(object sender, EventArgs e)
         {
+            // Highlight the report link in the navbar
+            const string currentItemCss = "nav-link disabled text-white";
+            const string otherItemsCss = "nav-link";
+            ((HyperLink)Master.FindControl("ReportLink")).CssClass = currentItemCss;
+            // Reset styling for other items
+            ((HyperLink)Master.FindControl("ScheduleLink")).CssClass = otherItemsCss;
+            ((HyperLink)Master.FindControl("AttendanceLink")).CssClass = otherItemsCss;
+
             int? studentId = Values.StudentId(Session, Request.Cookies);
             if (studentId == null)
             {
-                Response.Redirect("~/");
+                Response.Redirect("~/?to=Report");
             }
-
-            Student student = (Student)Session["student"];
-            // TODO: Populate from dropdown
-            SemesterData currentSemester = new SemesterData(1);
-            currentSemester.Sync();
-
+            Student student;
+            if (Session["student"] == null)
+            {
+                // this is somehow unavailable while studentId is available
+                // Reset it
+                student = new Student((int)studentId);
+                student.Sync();
+                Session["student"] = student;
+            }
+            else
+            {
+                student = (Student)Session["student"];
+            }
             ((Label)Master.FindControl("DisplayName")).Text = student.enrNo;
 
-            //SemesterGrades semesterGrades = new SemesterGrades((int)studentId, SemesterData.GetSemesterDataForDateTimeOffset(DateTimeOffset.Now).id);
-            SemesterGrades semesterGrades = new SemesterGrades(1, (int)studentId);
-            semesterGrades.Sync();
-            GV_Result.DataSource = semesterGrades.dataSet;
-            GV_Result.DataBind();
-            if (GV_Result.Rows.Count > 0)
+            // Setting values in the dropdown
+            Semester semesters = new Semester((int)studentId);
+            semesters.Sync();
+            SemesterSelect.Items.Clear();
+            foreach (SemesterData data in semesters.data)
             {
-                GV_Result.HeaderRow.TableSection = TableRowSection.TableHeader;
+                ListItem item = new ListItem();
+                item.Text = data.name;
+                item.Value = data.id.ToString();
+                SemesterSelect.Items.Add(item);
             }
-            RegCredits.Text = semesterGrades.regCreditSum.ToString();
-            Credits.Text = semesterGrades.creditSum.ToString();
-            Points.Text = semesterGrades.pointSum.ToString();
-            Sgpa.Text = String.Format("{0:0.00}", semesterGrades.pointSum / semesterGrades.creditSum);
 
-            StudentEnr.Text = student.enrNo;
-            StudentName.Text = student.name;
-            StudentSemester.Text = currentSemester.name;
+            SemesterData currentSemester = SemesterData.GetSemesterDataForDateTimeOffset(DateTimeOffset.Now);
+
+            if (!Page.IsPostBack)
+            {
+                // Default dropdown value is the current value
+                ListItem itemInDropdown = SemesterSelect.Items.FindByValue(currentSemester.id.ToString());
+                if (itemInDropdown != null)
+                {
+                    // Current semester is available in the dropdown
+                    itemInDropdown.Selected = true;
+                }
+                else
+                {
+                    // Current semester is not available in the dropdown
+                    // This may be because the current result has not been announced
+                    // In that case, add the item to dropdown, make it active, and show that no result is available
+                    // Also disable the print button
+                    ListItem item = new ListItem();
+                    item.Text = currentSemester.name;
+                    item.Value = currentSemester.id.ToString();
+                    item.Selected = true;
+                    SemesterSelect.Items.Add(item);
+                }
+                //Session["semester"] = currentSemester;
+            }
+            else
+            {
+                // Dropdown should have value
+                Debug.Print("New Value: " + Request.Form[SemesterSelect.UniqueID]);
+                // From https://claude.ai/share/d37aa874-c7fc-4c21-b087-365bbc6db515
+                SemesterSelect.Items.FindByValue(Request.Form[SemesterSelect.UniqueID]).Selected = true;
+            }
+
+            //SemesterData selectedSemester = (SemesterData)Session["semester"];
+            SemesterData selectedSemester = new SemesterData(Convert.ToInt32(Request.Form[SemesterSelect.UniqueID]));
+            selectedSemester.Sync();
+
+            PopulateResults(student, selectedSemester);
+
+            CourseRepeater.ItemDataBound += CourseRepeater_ItemDataBound;
+            List<Course> courses=LoadCourses();
+            CourseRepeater.DataSource = courses;
+            CourseRepeater.DataBind();
 
             Grades grades = new Grades();
             grades.Sync();
@@ -52,25 +107,49 @@ namespace NucleusProject
                 GradeExplanation.HeaderRow.TableSection = TableRowSection.TableHeader;
             }
 
-            if (!IsPostBack)
-            {
-                // Register ItemDataBound event
-                CourseRepeater.ItemDataBound += CourseRepeater_ItemDataBound;
+        }
 
-                // Load courses and bind to CourseRepeater
-                List<Course> courses = LoadCourses();
-                if (courses != null && courses.Count > 0)
-                {
-                    CourseRepeater.DataSource = courses;
-                    CourseRepeater.DataBind();
-                }
-                else
-                {
-                    Console.WriteLine("No courses available to bind.");
-                }
+        private void PopulateResults(Student student, SemesterData currentSemester)
+        {
+            SemesterGrades semesterGrades = new SemesterGrades(currentSemester.id, (int)student.id);
+            semesterGrades.Sync();
+            GV_Result.DataSource = semesterGrades.dataSet;
+            GV_Result.DataBind();
+            if (GV_Result.Rows.Count > 0)
+            {
+                GV_Result.HeaderRow.TableSection = TableRowSection.TableHeader;
+                PrintReport.Enabled = true;
+            }
+            else
+            {
+                PrintReport.Enabled = false;
+            }
+            RegCredits.Text = semesterGrades.regCreditSum.ToString();
+            Credits.Text = semesterGrades.creditSum.ToString();
+            Points.Text = semesterGrades.pointSum.ToString();
+            if (semesterGrades.creditSum != 0)
+            {
+                Sgpa.Text = String.Format("{0:0.00}", semesterGrades.pointSum / semesterGrades.creditSum);
+            }
+            else
+            {
+                Sgpa.Text = "0";
             }
 
+            StudentEnr.Text = student.enrNo;
+            StudentName.Text = student.name;
+            StudentSemester.Text = currentSemester.name;
 
+            OverallGrades overallGrades = new OverallGrades((int)student.id);
+            overallGrades.Sync();
+
+            CumRegCredits.Text = overallGrades.regCreditSum.ToString();
+            CumCredits.Text = overallGrades.creditSum.ToString();
+            CumPoints.Text = overallGrades.pointSum.ToString();
+            if (overallGrades.creditSum != 0)
+            {
+                Cgpa.Text = String.Format("{0:0.00}", overallGrades.pointSum / overallGrades.creditSum);
+            }
         }
 
         private List<Course> LoadCourses()
@@ -283,35 +362,6 @@ namespace NucleusProject
             public string PublishedDate { get; set; }
             public string PublishedBy { get; set; }
         }
-
-        protected string ResolvePath(string path)
-        {
-            try
-            {
-                return Server.MapPath(path);
-            }
-            catch (HttpException)
-            {
-                // Happens with concrete paths
-                return path;
-            }
-        }
-
-        protected void DownloadFile()
-        {
-
-            // Adapted from https://stackoverflow.com/a/21417259
-            string file = ResolvePath(this.path);
-            System.IO.FileInfo fileInfo = new System.IO.FileInfo(file);
-            Response.ContentType = "application/pdf";
-            Response.AppendHeader("Content-Disposition", "attatchment;filename=report.pdf");
-            Response.AppendHeader("Content-Length", fileInfo.Length.ToString());
-            Response.TransmitFile(file);
-            // Probably unecessary, keeping it anyway
-            Response.Flush();
-            Response.End();
-        }
-
     }
 
 }
